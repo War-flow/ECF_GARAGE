d31c2d46357ffe19ba30801ac0e16db83d4ba059
Cinomme <derka161@gmail.com>
2023-09-11 23:46:18 -0300

    Créarion page avis

--- src/Controller/AvisController.php
+++ src/Controller/AvisController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AvisRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class AvisController extends AbstractController
 {
     #[Route('/avis', name: 'app_avis')]
-    public function index(): Response
+    public function index(AvisRepository $avisRepository): Response
     {
         return $this->render('avis/index.html.twig', [
-            'controller_name' => 'AvisController',
+            'avis' => $avisRepository->findBy([],[])
         ]);
     }
 }
---
+++ src/DataFixtures/AvisFixture.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Avis;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
+
+class AvisFixture extends Fixture
+{
+
+    private $counters = 1;
+
+    /**
+     * var Generator
+     */
+    private Generator $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        
+        for ($pls = 1; $pls <= 10; $pls++) {
+            $avis = new Avis();
+            $avis->setName($this->faker->text(5));
+            $avis->setMessage($this->faker->sentence(5));
+            $avis->setNote($this->faker->numberBetween(1, 5));
+            $avis->setValidation(true);
+            $manager->persist($avis);
+        }
+        $manager->flush();
+    }
+}
